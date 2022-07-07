ec75b50107612485ce8fd1f9c994d2e3e397ad09
Dvlpr Lya <108205878+Dvlprly@users.noreply.github.com>
2022-07-07 21:32:53 +0900

    Update main.dart

--- main.dart
+++ main.dart
@@ -14,24 +14,19 @@ class MyProfile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('JMB'),
-        centerTitle: true,
-        backgroundColor: Colors.redAccent,
-        elevation: 0.0, // AppBar 밑에 그림자 제거
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment:
-              MainAxisAlignment.center, // 위젯들을 세로 상중하 정렬할 때 사용(중심축 정렬)
-          children: const [
-            Text(''),
-            Text(''),
-            Text(''),
-          ],
+   return Scaffold(
+        backgroundColor: Colors.amber[800],
+        appBar: AppBar(
+          title: const Text('JMB'),
+          centerTitle: true,
+          backgroundColor: Colors.amber[700],
+          elevation: 0.0, // AppBar 밑에 그림자 제거
         ),
-      ),
-    );
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
+          child: Column(
+            children: [Text('NAME')],
+          ),
+        ));
   }
 }

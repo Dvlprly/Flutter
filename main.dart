ffd1db266dc5acad8808bbf9b5c1f84270cb410c
Dvlpr Lya <108205878+Dvlprly@users.noreply.github.com>
2022-07-10 20:06:02 +0900

    Update main.dart

--- main.dart
+++ main.dart
@@ -15,11 +15,11 @@ class MyProfile extends StatelessWidget {
     @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.amber[800],
+        backgroundColor: Colors.amber[900],
         appBar: AppBar(
           title: const Text('JMB'),
           centerTitle: true,
-          backgroundColor: Colors.amber[700],
+          backgroundColor: Colors.amber[800],
           elevation: 0.0, // AppBar 밑에 그림자 제거
         ),
         body: Padding(
